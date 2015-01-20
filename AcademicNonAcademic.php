85e9eeda05c15c8456781cae45c2b482eab591ee
gastonvelarde <iamgastonvelarde@gmail.com>
2015-01-19 16:06:04 -1100

    Displaying of employees from the database

---
+++ AcademicNonAcademic.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <!-- This file has been downloaded from Bootsnipp.com. Enjoy! -->
+    <title>Academic Non-Teaching & Non-Academic Personnel</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600"
+        rel="stylesheet">
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/pages/dashboard.css" rel="stylesheet">
+
+    <!--CSS code for profiles-->
+    <style type="text/css">
+        .user-row {
+         margin-bottom: 14px;
+        }
+
+        .user-row:last-child {
+            margin-bottom: 0;
+        }
+
+        .dropdown-user {
+            margin: 13px 0;
+            padding: 5px;
+            height: 100%;
+        }
+
+        .dropdown-user:hover {
+            cursor: pointer;
+        }
+
+        .table-user-information > tbody > tr {
+            border-top: 1px solid rgb(221, 221, 221);
+        }
+
+        .table-user-information > tbody > tr:first-child {
+            border-top: 0;
+        }
+
+
+        .table-user-information > tbody > tr > td {
+            border-top: 0;
+        }
+
+    </style>
+
+     <!--CSS code for profiles-->
+
+      <!--Javascript code for profiles-->
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <!--Javascript code for profiles-->
+</head>
+<body>
+
+<div class="navbar navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span
+                    class=></span><span class="icon-bar"></span><span class="icon-bar"></span> </a><a class="brand" href="index.html">LBASS Human Resource Information System </a>
+      <div class="nav-collapse">
+        <ul class="nav pull-right">
+      
+            <ul class="dropdown-menu">
+              <li><a href="javascript:;">Settings</a></li>
+              <li><a href="javascript:;">Help</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i
+                            class="icon-user"></i> Let Rivera (Dummy Data)<b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="javascript:;">Profile</a></li>
+              <li><a href="javascript:;">Logout</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="navbar-search pull-right">
+          <input type="text" class="search-query" placeholder="Search">
+        </form>
+      </div>
+      <!--/.nav-collapse --> 
+    </div>
+    <!-- /container --> 
+  </div>
+  <!-- /navbar-inner --> 
+</div>
+<!-- /navbar -->
+<div class="subnavbar">
+  <div class="subnavbar-inner">
+    <div class="container">
+      <ul class="mainnav">
+        <li class="active"><a href="index.html"><i class="icon-dashboard"></i><span>HR Dashboard</span> </a> </li>
+        <li><a href="EmployeesPage.php"><i class="icon-user"></i><span>Employees</span> </a> </li>
+        
+        <li><a href="ReportsPage.php"><i class="icon-list-alt"></i><span>Reports</span> </a> </li>
+        <li><a href="guidely.html"><i class="icon-table"></i><span>Attendance</span> </a></li>
+        <li><a href="charts.html"><i class="icon-bar-chart"></i><span>Charts</span> </a> </li>
+        <li><a href="shortcodes.html"><i class="icon-code"></i><span>Shortcodes</span> </a> </li>
+        <li class="dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-long-arrow-down"></i><span>Drops</span> <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="icons.html">Icons</a></li>
+            <li><a href="faq.html">FAQ</a></li>
+            <li><a href="pricing.html">Pricing Plans</a></li>
+            <li><a href="login.html">Login</a></li>
+            <li><a href="signup.html">Signup</a></li>
+            <li><a href="error.html">404</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <!-- /container --> 
+  </div>
+  <!-- /subnavbar-inner --> 
+</div>
+<!-- /subnavbar -->
+
+
+<!--Container for Employee Profiles-->
+
+<div class="container">
+    <div class="row">
+        <div class="span12">
+            <ul class="thumbnails">
+                <li class="span5 clearfix">
+                  <div class="thumbnail clearfix">
+                    <img src="http://placehold.it/320x200" alt="ALT NAME" class="pull-left span2 clearfix" style='margin-right:10px'>
+                    <div class="caption" class="pull-left">
+                      <a href="http://bootsnipp.com/" class="btn btn-primary icon  pull-right">Select</a>
+                      <h4>      
+                      <a href="#" >Luis Felipe Kaufmann</a>
+                      </h4>
+                      <small><b>RG: </b>99384877</small>  
+                    </div>
+                  </div>
+                </li>
+                  <li class="span5 clearfix">
+                  <div class="thumbnail clearfix">
+                    <img src="http://placehold.it/320x200" alt="ALT NAME" class="pull-left span2 clearfix" style='margin-right:10px'>
+                    <div class="caption" class="pull-left">
+                      <a href="http://bootsnipp.com/" class="btn btn-primary icon  pull-right">Select</a>
+                      <h4>      
+                      <a href="#" >Luis Felipe Kaufmann</a>
+                      </h4>
+                      <small><b>RG: </b>99384877</small>  
+                    </div>
+                  </div>
+                </li>
+                  <li class="span5 clearfix">
+                  <div class="thumbnail clearfix">
+                    <img src="http://placehold.it/320x200" alt="ALT NAME" class="pull-left span2 clearfix" style='margin-right:10px'>
+                    <div class="caption" class="pull-left">
+                      <a href="http://bootsnipp.com/" class="btn btn-primary icon  pull-right">Select</a>
+                      <h4>      
+                      <a href="#" >Luis Felipe Kaufmann</a>
+                      </h4>
+                      <small><b>RG: </b>99384877</small>  
+                    </div>
+                  </div>
+                </li>
+                  <li class="span5 clearfix">
+                  <div class="thumbnail clearfix">
+                    <img src="http://placehold.it/320x200" alt="ALT NAME" class="pull-left span2 clearfix" style='margin-right:10px'>
+                    <div class="caption" class="pull-left">
+                      <a href="http://bootsnipp.com/" class="btn btn-primary icon  pull-right">Select</a>
+                      <h4>      
+                      <a href="#" >Luis Felipe Kaufmann</a>
+                      </h4>
+                      <small><b>RG: </b>99384877</small>     
+                    </div>
+                  </div>
+                </li>
+                  <li class="span5 clearfix">
+                  <div class="thumbnail clearfix">
+                    <img src="http://placehold.it/320x200" alt="ALT NAME" class="pull-left span2 clearfix" style='margin-right:10px'>
+                    <div class="caption" class="pull-left">
+                      <a href="http://bootsnipp.com/" class="btn btn-primary icon  pull-right">Select</a>
+                      <h4>      
+                      <a href="#" >Luis Felipe Kaufmann</a>
+                      </h4>
+                      <small><b>RG: </b>99384877</small>
+                    </div>
+                  </div>
+                </li>
+                  <li class="span5 clearfix">
+                  <div class="thumbnail clearfix">
+                    <img src="http://placehold.it/320x200" alt="ALT NAME" class="pull-left span2 clearfix" style='margin-right:10px'>
+                    <div class="caption" class="pull-left">
+                      <a href="http://bootsnipp.com/" class="btn btn-primary icon  pull-right">Select</a>
+                      <h4>      
+                      <a href="#" >Luis Felipe Kaufmann</a>
+                      </h4>
+                      <small><b>RG: </b>99384877</small>
+                    </div>
+                  </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+
+</script>
+
+</body>
+</html>
