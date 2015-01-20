dc8ac0367cac49dc3717a522615a69edccbbd8fb
Larissa Ubungen <larissaubungen@gmail.com>
2015-01-21 00:51:14 +0800

    ApplicationSubmitSuccess and Failed
    for the end page of resume submission.

---
+++ ApplicantSubmitSuccess.php
@@ -0,0 +1,81 @@
+<html>
+<head>
+	<meta charset="utf-8">
+        <title>LBASS Applicant Resume: success</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600"
+                rel="stylesheet">
+        <link href="css/font-awesome.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/pages/dashboard.css" rel="stylesheet">
+
+        <link rel="stylesheet" href="jquery/jquery-ui.css">
+        <link href="content/shared/styles/examples-offline.css" rel="stylesheet">
+
+	<script src="js/jquery-1.7.2.min.js"></script> 
+    <script src="js/excanvas.min.js"></script> 
+    <script src="js/chart.min.js" type="text/javascript"></script> 
+    <script src="js/bootstrap.js"></script>
+    <script src="js/base.js"></script>   
+
+	<title>LBASS Applicant Resume: submitted</title>
+</head>
+<body>
+
+  <div class="navbar navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span
+                    class=></span><span class="icon-bar"></span><span class="icon-bar"></span> </a><a class="brand" href="#">LBASS Human Resource Information System </a>
+      <div class="nav-collapse">
+        <ul class="nav pull-right">
+      
+            <ul class="dropdown-menu">
+              <li><a href="javascript:;">Settings</a></li>
+              <li><a href="javascript:;">Help</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i
+                            class="icon-user"></i> Let Rivera (Dummy Data)<b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="javascript:;">Profile</a></li>
+              <li><a href="javascript:;">Logout</a></li>
+            </ul>
+          </li>
+        </ul>
+            <form class="navbar-search pull-right">
+              <input type="text" class="search-query" placeholder="Search">
+            </form>
+          </div>
+          <!--/.nav-collapse --> 
+        </div>
+        <!-- /container --> 
+      </div>
+      <!-- /navbar-inner --> 
+    </div>
+    <!-- /navbar -->
+    <div class="subnavbar">
+      <div class="subnavbar-inner">
+        <div class="container">
+          <ul class="mainnav">
+            <li class="active"><a href="#"><i class="icon-home"></i><span>Home</span> </a> </li>
+            <li><a href="ApplicantGuidelines.html"><i class="icon-list-alt"></i><span>Guidelines</span> </a> </li>
+            <li><a href="ApplicantResume.php"><i class="icon-file"></i><span>Resume</span> </a> </li>      
+          </ul>
+        </div>
+        <!-- /container --> 
+      </div>
+      <!-- /subnavbar-inner --> 
+    </div>
+    <!-- /subnavbar -->
+
+    <div class="alert alert-success" role="alert">
+
+  		<div class="alert-link"> <b> Congratulations, Applicant Name!</b>
+  		You have submitted your resume.</div>
+	</div>
+</body>
+</html>
